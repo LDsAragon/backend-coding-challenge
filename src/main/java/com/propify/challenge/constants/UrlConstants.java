5599f69b63ce1c1052d51eb765cd0bb5ad945319
Matias Salomone <matisalomone@hotmail.com>
2022-02-02 15:09:20 -0300

    Commit: updatingController

--- src/main/java/com/propify/challenge/constants/UrlConstants.java
+++ src/main/java/com/propify/challenge/constants/UrlConstants.java
@@ -2,5 +2,12 @@ package com.propify.challenge.constants;
 
 public class UrlConstants {
 
+    public static final String PROPERTY_CONTROLLER = "/property" ;
+    public static final String SEARCH_BETWEEN = "/searchBetween/{minRentPrice}/and/{maxRentPrice}" ;
+    public static final String SEARCH_BY_ID = "/searchById/{id}" ;
+    public static final String CREATE_PROPERTY = "/create" ;
+    public static final String UPDATE_PROPERTY = "/update" ;
+    public static final String DELETE_PROPERTY = "/delete/{id}" ;
+    public static final String REPORT_PROPERTIES = "/reportProperties" ;
 
 }
--- src/main/java/com/propify/challenge/controller/PropertyController.java
+++ src/main/java/com/propify/challenge/controller/PropertyController.java
@@ -1,52 +1,53 @@
 package com.propify.challenge.controller;
 
+import com.propify.challenge.constants.UrlConstants;
 import com.propify.challenge.entities.Property;
 import com.propify.challenge.entities.PropertyReport;
 import com.propify.challenge.service.PropertyService;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.Collection;
 
 /**
  *  API endpoints for CRUD operations on entities of type Property
  * */
 
-@RestController
+@RestController(UrlConstants.PROPERTY_CONTROLLER)
 public class PropertyController {
 
     @Autowired
     PropertyService propertyService;
 
-    @GetMapping(value = "")
+    @GetMapping(value = UrlConstants.SEARCH_BETWEEN)
     public Collection<Property> search(String minRentPrice, String maxRentPrice) {
         return propertyService.search(minRentPrice, maxRentPrice);
     }
 
-    @GetMapping(value = "")
+    @GetMapping(value = UrlConstants.SEARCH_BY_ID)
     public Property findById(int id) {
         return propertyService.findById(id);
     }
 
-    @PostMapping(value = "")
-    public void insert(@Valid Property property) {
-        // TODO: Property attributes must be validated
+    @PostMapping(value = UrlConstants.CREATE_PROPERTY)
+    public void insert(@Validated(Insert.class) Property property) {
         propertyService.insert(property);
     }
 
-    @PutMapping(value = "")
-    public void update(@Valid Property property) {
-        // TODO: Property attributes must be validated
+    @PutMapping(value = UrlConstants.UPDATE_PROPERTY)
+    public void update(@Validated(Update.class) Property property) {
         propertyService.update(property);
     }
 
-    @DeleteMapping(value = "")
+    @DeleteMapping(value = UrlConstants.DELETE_PROPERTY)
     public void delete(int id) {
         propertyService.delete(id);
     }
 
-    @GetMapping(value = "")
+    @GetMapping(value = UrlConstants.REPORT_PROPERTIES)
     public PropertyReport report() {
         return propertyService.propertyReport();
     }
