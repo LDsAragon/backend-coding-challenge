12ca3ec407060bcf76a90bedb1a21da0d6638298
Matias Salomone <matisalomone@hotmail.com>
2022-02-02 14:55:41 -0300

    Commit: regexp

--- src/main/java/com/propify/challenge/constants/Validations.java
+++ src/main/java/com/propify/challenge/constants/Validations.java
@@ -2,9 +2,34 @@ package com.propify.challenge.constants;
 
 public class Validations {
 
-    public final static String EMAIL_VALIDATION_PROPERTY = "" ;
-    public final static String CODE_VALIDATION_PROPERTY = "" ;
+    /**
+     *  Validate correct email format
+     * */
+    public final static String EMAIL_VALIDATION_PROPERTY = "/^w+[+.w-]*@([w-]+.)*w+[w-]*.([a-z]{2,4}|d+)$/i" ;
+
+    /**
+     *  Validate numbers and capital letters in any combination until 10 characters.
+     * */
+    public final static String CODE_VALIDATION_PROPERTY = "^[A-Z0-9]{0,10}" ;
+
+    /**
+     *  Validate the word "UTF" in capital letters
+     *  Followed by plus or minus sign
+     *  And a number ranging from 0 to 9.
+     * */
+    public final static String TIMEZONE_VALIDATION_ADDRESS = "/\\bUTF\\b(\\-|\\+)([0-9])/g" ;
+
+    /**
+     *  Validate 5 digits
+     *  Ranging from 0 to 9.
+     * */
+    public final static String STATE_VALIDATION_ADDRESS = "^[0-9]{5}" ;
+
+    /**
+     *  Validate 2 letters
+     * */
+    public final static String ZIP_VALIDATION_ADDRESS = "^[A-Za-z]{2}" ;
 
 
-    // public final static String EMAIL_VALIDATION_PROPERTY = "" ;
+    // public final static String STATE_VALIDATION_ADDRESS = "" ;
 }
--- src/main/java/com/propify/challenge/entities/Address.java
+++ src/main/java/com/propify/challenge/entities/Address.java
@@ -1,15 +1,39 @@
 package com.propify.challenge.entities;
 
 
+import com.propify.challenge.constants.Validations;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Update;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Pattern;
+
 public class Address {
 
-    public String street; // must not be null or blank
+    @NotNull(groups = {Update.class}) // must be not null for UPDATE
+    @Null(groups = {Insert.class}) // must be null for INSERT
+    public int id;
 
-    public String city; // must not be null or blank
+    @NotBlank // must not be null or blank
+    public String street;
 
-    public String state; // 2-letter code, must not be null or blank
+    @NotBlank // must not be null or blank
+    public String city;
 
-    public String zip; // 5-digit code, must not be null or blank
+    @NotBlank // must not be null or blank
+    @Length(min = 2 ,max = 2) // 2-letter code
+    @Pattern(regexp = Validations.STATE_VALIDATION_ADDRESS)
+    public String state; //
 
+    @NotBlank // must not be null or blank
+    @Length(min = 5 ,max = 5) // 5-digit code
+    @Pattern(regexp = Validations.ZIP_VALIDATION_ADDRESS)
+    public String zip;
+
+    @NotBlank // must not be null or blank
+    @Pattern(regexp = Validations.TIMEZONE_VALIDATION_ADDRESS)
     public String timezone; // Must be a valid timezone
 }
